4a3c90ca975653446d2bf08ddead615da4f108de
Gavin de Bruyn <gavin.debruyn1@gmail.com>
2021-11-29 19:42:43 +0200

    Add bootstrap to application and create file to theme application.
    Signed-off-by: Gavin de Bruyn <gavin.debruyn1@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import './styling/styles.scss'
 
 ReactDOM.render(
     <React.StrictMode>
